87003eae3004e18170194121a0e16d7314e294e1
Your Name <morvaridmahmoudi@gmail.com>
2022-04-14 02:00:50 +0430

    shopping

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer(){
+    return(
+     <footer>
+    <span>
+      <p className="copy">
+        &COPY; Copyright 2018 World Wide Marketing, Copyright Registration.
+      </p>
+    </span>
+  </footer>
+    )
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+function Header(){
+    return(
+        
+    <header>
+     <div className="title">
+      <a href="{}">Women</a>
+      <a href="{}">Men</a>
+      <a href="{}">Children</a>
+    </div>
+    <div className="logo">
+      <h1>Online-Shopping</h1>
+    </div>
+    <div className="search">
+      <input type="text" value="search" id="search" placeholder="Search" />
+      <button class="btn" type="submit">Search</button>
+    </div> 
+  </header>
+
+
+    
+
+    )
+};
+
+
+
+
+export default Header;
